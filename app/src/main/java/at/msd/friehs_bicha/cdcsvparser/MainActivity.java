3317df14d68f24b296008f74fa7a7d681390f924
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2022-12-05 20:07:09 +0100

    [TASK] added values to spinner

--- app/src/main/java/at/msd/friehs_bicha/cdcsvparser/MainActivity.java
+++ app/src/main/java/at/msd/friehs_bicha/cdcsvparser/MainActivity.java
@@ -3,6 +3,8 @@ package at.msd.friehs_bicha.cdcsvparser;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.spinner1);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 }
