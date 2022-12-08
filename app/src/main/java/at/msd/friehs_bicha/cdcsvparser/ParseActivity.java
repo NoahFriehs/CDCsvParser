f74018512d08dceb556b87c850fe25c23f77dfc5
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2022-12-08 14:16:03 +0100

    [TASK] commit for merging conflict solve

---
+++ app/src/main/java/at/msd/friehs_bicha/cdcsvparser/ParseActivity.java
@@ -0,0 +1,32 @@
+package at.msd.friehs_bicha.cdcsvparser;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class ParseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parse);
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
