566a2e1cf877d16c727d319ca422f62a5830dec5
Stefan Bicha <stefan.bicha@edu.fh-joanneum.at>
2023-06-20 12:49:37 +0200

    [TASK] bugfixes

--- app/src/main/java/at/msd/friehs_bicha/cdcsvparser/ui/activity/AboutUsActivity.kt
+++ app/src/main/java/at/msd/friehs_bicha/cdcsvparser/ui/activity/AboutUsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import at.msd.friehs_bicha.cdcsvparser.R
@@ -22,15 +23,20 @@ class AboutUsActivity : AppCompatActivity() {
 
         val emailButton = findViewById<TextView>(R.id.tv_mailString)
 
-        emailButton.setOnClickListener {
-            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:cdcsvparser@gmail.com")
-            }
 
-            // Ensure there's an app to handle this intent
-            if (emailIntent.resolveActivity(packageManager) != null) {
-                startActivity(emailIntent)
+        emailButton.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val emailIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "message/rfc822"
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("cdcsvparser@gmail.com"))
+                }
+
+                // Ensure there's an app to handle this intent
+                if (emailIntent.resolveActivity(packageManager) != null) {
+                    startActivity(emailIntent)
+                }
             }
+            true
         }
 
     }
