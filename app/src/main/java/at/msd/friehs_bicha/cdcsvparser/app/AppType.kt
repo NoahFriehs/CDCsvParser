98ff2d3edb779ee8b9bdba937d33db15764c3978
n.friehs <noah.friehs@edu.fh-joanneum.at>
2023-12-15 09:53:03 +0100

    Update Enum

--- app/src/main/java/at/msd/friehs_bicha/cdcsvparser/app/AppType.kt
+++ app/src/main/java/at/msd/friehs_bicha/cdcsvparser/app/AppType.kt
@@ -4,5 +4,5 @@ package at.msd.friehs_bicha.cdcsvparser.app
  * App type enum
  */
 enum class AppType {
-    CdCsvParser, CroCard, CurveCard, Default
+    CdCsvParser, CroCard, CurveCard, Default, Custom
 }
